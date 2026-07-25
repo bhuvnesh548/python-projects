232502763cf5a76ec40be45dd05a0897dcf8c04a
bhuvnesh <bhuvneshpandey23@gmail.com>
2026-07-25 17:53:33 +0530

    Simple number guessing game
    if your number is matches with the number that is guessed by computer then you won !

---
+++ number guessing game.py
@@ -0,0 +1,27 @@
+#number guessing game 
+import random
+def getRandomNumber():
+    return random.randrange(1, 100)
+
+def giveHint(number, guess):
+    if guess > (number + 5) or guess < (number - 5):
+        return "Far away "
+    elif number == guess:
+        return "Right"
+    else:
+        return "So close "
+
+def runGuess():
+    secretNumber = getRandomNumber()
+    # Update the code below
+    while True:
+        user_guess = int(input("Enter a number between 1 and 100: "))
+        hint = giveHint(secretNumber, user_guess)
+        if hint == "Right":
+            print("You guessed it Right!")
+            break    
+        else:
+            print(hint)
+            
+if __name__ == '__main__':
+    runGuess()
