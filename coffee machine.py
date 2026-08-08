172947f01df1e8f70124657ae1704a3553f5dd7a
bhuvnesh <bhuvneshpandey23@gmail.com>
2026-08-08 20:04:44 +0530

    Update coffee machine.py

--- coffee machine.py
+++ coffee machine.py
@@ -1,4 +1,4 @@
-#coffe machine 
+#coffee machine 
 menu={"espresso":{"ingredients":{"water":150,"coffee":25},"cost":10},
       "latte":{"ingredients":{"water":75,"milk":75,"coffee":25},"cost":15},
       "capucchino":{"ingredients":{"water":50,"milk":100,"coffee":25},"cost":20}
@@ -17,6 +17,7 @@ def resource_sufficient(ingredient):
         else:
             resources[item]=resources[item]-ingredient[item]
 
+
 print(f"{"welcome to the coffee machine":-^100}")
 ison=True
 while ison:
@@ -30,6 +31,12 @@ while ison:
         print("profit : ",profit,"₹")
 
     elif choice=="add":
+        remained_water=resources["water"]
+        remained_milk=resources["milk"]
+        remained_coffee=resources["coffee"] 
+        print(f"remaining water : {remained_water} ml")
+        print(f"remaining milk : {remained_milk} ml") 
+        print(f"remaining coffee : {remained_coffee} g")
         wate=int(input("fill water"))
         resources["water"]+=wate
         mil=int(input("fill milk"))
