535f2d1519e6ea5c36aa86a66a50208cdb0d4333
bhuvnesh <bhuvneshpandey23@gmail.com>
2026-08-16 22:49:17 +0530

    Initial Commit

--- money bank/bank.py
+++ money bank/bank.py
@@ -1,7 +1,10 @@
 from getpass import getpass
 import pwinput
 def create_account(name,mobile,address):
-    pass
+    info=[name,mobile,address]
+    for i in info:
+        with open("info.csv","a")as f:
+            f.write(str(i) + ",")
 def credit(number,amt,passw):
     pass
 def debit(number,amt):
@@ -10,20 +13,21 @@ def view(number,amt):
     pass
 
 print(f"{"| Money Machine |":~^50}")
-option=print("select a option : 1.credit/2.debit/3.view statement/4.create Account")
-if option==1:
+option=input("select a option : 1.credit/2.debit/3.view statement/4.create Account")
+if option=="1":
     account= 1234567890#int(input("enter the account number: "))
     amount= 1000#int(input("enter amount: "))
     password = 12345 #pwinput.pwinput(prompt='Enter password: ', mask='⁕')
-elif option==2:
+elif option=="2":
     account=1234567890
     amount=1000
-elif option==3:
+elif option=="3":
     account=1234567890
     password=12345
-elif option==4:
+elif option=="4":
     name="bhuvnesh"
     mobile=1234567890
     address="bareilly"
+    create_account(name,mobile,address)
 else:
     print("select a valid option !")
